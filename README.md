37178a381ef6c8b726bffcc3ac79bf3b57038273
Vidar Eldøy <vidar@eldoy.com>
2023-10-05 22:29:04 +0200

    Initial build

---
+++ README.md
@@ -0,0 +1,20 @@
+# Dcountry
+
+Data about countries. Based on [country data list](https://github.com/Sonatrix/country-list) but stores everything in a single JSON file.
+
+Useful for building your own subsets or databases.
+
+### Install
+
+```
+npm i dcountry
+```
+
+### Usage
+
+```js
+// Bring out the list of countries
+var { countries } = require('dcountry')
+```
+
+MIT Licensed. Enjoy!
---
+++ scripts/build.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+var fs = require('node:fs')
+var list = require('country-data-list')
+var countries = list.countries.all
+var continents = list.continents
+var languages = list.languages
+var regions = list.regions
+
+var data = []
+for (var key in countries) {
+  var country = countries[key]
+  country.iso2 = (country.alpha2 || '').toUpperCase()
+  country.iso3 = (country.alpha3 || '').toUpperCase()
+  country.calls = country.countryCallingCodes
+  delete country.alpha2
+  delete country.alpha3
+  delete country.countryCallingCodes
+
+  country.lang = country.languages.map((lang) => {
+    var language = languages[lang]
+    language.bibliographic = (language.bibliographic || '').toUpperCase()
+    language.iso2 = (language.alpha2 || '').toUpperCase()
+    language.iso3 = (language.alpha3 || '').toUpperCase()
+    delete language.alpha2
+    delete language.alpha3
+    return language
+  })
+  delete country.languages
+  data.push(countries[key])
+}
+
+// Add continents
+for (var key in continents) {
+  var continent = continents[key]
+  for (var code of continent.countries) {
+    var country = data.find((x) => x.iso2 == code)
+    country.continent = continent.name
+  }
+}
+
+// Add regions
+for (var name in regions) {
+  var region = regions[name]
+  console.log(region)
+  for (var code of region.countries) {
+    var country = data.find((x) => x.iso2 == code)
+    country.region = region.name
+  }
+}
+
+var result = JSON.stringify(data, null, 2)
+fs.writeFileSync('./countries.json', result)
